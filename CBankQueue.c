4db53f3a1cae7ffe4cdff05a36aaa88c7e3fe04d
jonathanbeber <jonathanbeber@gmail.com>
2016-06-07 13:54:38 -0300

    insert in queue

--- CBankQueue.c
+++ CBankQueue.c
@@ -11,3 +11,22 @@ struct no {
 typedef no *ponteiro_de_no;
 ponteiro_de_no fila;
 int operacao;
+
+void fila_inserir(ponteiro_de_no fila){
+    char categoria;
+    int contador_de_categoria = 0;
+
+    mostra_categorias();
+    scanf("%c", &categoria);
+
+    while(fila->proximo != NULL){
+        if(fila->categoria == categoria){
+            contador_de_categoria++;
+        }
+        fila = fila->proximo;
+    }
+    fila->proximo = (ponteiro_de_no) malloc(sizeof(no));
+    fila = fila->proximo;
+    fila->sequencia = ++contador_de_categoria;
+    fila->proximo = NULL;
+}
